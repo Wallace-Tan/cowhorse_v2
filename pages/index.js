ea712c3f30431ec8c949eeedd9de0e3f0b040a23
Wallace-Tan <wallacetan1125@gmail.com>
2026-03-01 17:21:55 +0800

    test: deploy to swa

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,7 @@ const Home = () => (
   <div className="home">
     <h1>What Can I Deploy to Static Apps?</h1>
     <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
+      hello world
     </div>
   </div>
 );
